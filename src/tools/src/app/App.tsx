848a8597b4e09d53615d72257eb38c98fa274c76
Roman Puzanov <puzanovrs@gmail.com>
2021-09-19 15:10:11 +0500

    Подключил mdb-react-ui-kit

--- src/tools/src/app/App.tsx
+++ src/tools/src/app/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { Header } from './Header';
 import { Router } from './Router';
