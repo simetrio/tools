ad5acea8873478acda1361a230b1de8a5d045449
Roman Puzanov <puzanovrs@gmail.com>
2021-09-19 17:06:36 +0500

    Генерация Guid

--- src/tools/src/tools/guid-generator/GuidGenerator.tsx
+++ src/tools/src/tools/guid-generator/GuidGenerator.tsx
@@ -1,5 +1,86 @@
+import { MDBBtn, MDBCheckbox, MDBCol, MDBInput } from "mdb-react-ui-kit";
+import { useState } from "react";
+import { GuidGeneratorUtils } from "./GuidGeneratorUtils";
+
+interface FormValue {
+    count: number,
+    uppercase: boolean,
+    braces: boolean,
+    hypens: boolean,
+    value: string[],
+}
+
 export const GuidGenerator: React.FC = () => {
+    const [formValue, setFormValue] = useState<FormValue>({
+        count: 1,
+        uppercase: false,
+        braces: false,
+        hypens: true,
+        value: [],
+    });
+
+    const onChange = (e: any) => {
+        setFormValue({ ...formValue, [e.target.name]: e.target.value });
+    };
+
+    const onChangeBoolean = (e: any) => {
+        setFormValue({ ...formValue, [e.target.name]: e.currentTarget.checked });
+    };
+
+    const onGenerate = () => {
+        setFormValue({ ...formValue, value: GuidGeneratorUtils.generate(formValue) });
+    }
+
     return (
-        <div>Guid generator</div>
+        <>
+            <h1>Online Guid generator</h1>
+            <MDBCol md="4" className="mb-3">
+                <MDBInput
+                    value={formValue.count.toString()}
+                    name="count"
+                    id="count"
+                    label="How many?"
+                    onChange={onChange}
+                />
+            </MDBCol>
+
+            <MDBCol md="4" className="mb-3">
+                <MDBCheckbox
+                    name="uppercase" 
+                    id="uppercase" 
+                    label="Uppercase"
+                    onClick={onChangeBoolean}
+                    defaultChecked={formValue.uppercase}
+                />
+            </MDBCol>
+
+            <MDBCol md="4" className="mb-3">
+                <MDBCheckbox
+                    name="braces" 
+                    id="braces" 
+                    label="Braces"
+                    onClick={onChangeBoolean}
+                    defaultChecked={formValue.braces}
+                />
+            </MDBCol>
+
+            <MDBCol md="4" className="mb-3">
+                <MDBCheckbox
+                    name="hypens"
+                    id="hypens" 
+                    label="Hypens"
+                    onClick={onChangeBoolean}
+                    defaultChecked={formValue.hypens}
+                />
+            </MDBCol>
+
+            <MDBCol md="4" className="mb-3">
+                <MDBBtn onClick={onGenerate}>Generate</MDBBtn>
+            </MDBCol>
+
+            <MDBCol md="4" className="mb-3">
+                <MDBInput label="Generated Guids" textarea rows={10} value={formValue.value.join("\n")} />
+            </MDBCol>
+        </>
     )
 }
---
+++ src/tools/src/tools/guid-generator/GuidGeneratorUtils.ts
@@ -0,0 +1,38 @@
+interface Props {
+    count: number,
+    uppercase: boolean,
+    braces: boolean,
+    hypens: boolean,
+}
+
+export const GuidGeneratorUtils = {
+    generate: (props: Props): string[] => {
+        const guids: string[] = [];
+        for(let i = 0; i < props.count; i++) {
+            guids.push(newGuid(props));
+        }
+        return guids;
+    }
+}
+
+function newGuid(props: Props): string {
+    let guid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 || 0;
+        const v = c === "x" ? r : (r & 0x3 || 0x8);
+        return v.toString(16);
+    });
+
+    if (props.uppercase) {
+        guid = guid.toUpperCase();
+    }
+
+    if (props.braces) {
+        guid = `{${guid}}`;
+    }
+
+    if (!props.hypens) {
+        guid = guid.replace(/-/g, "");
+    }
+
+    return guid;
+}
