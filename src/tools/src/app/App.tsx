63cdce48fdc9443d818768d1cdcc63ec3bd56835
Roman Puzanov <puzanovrs@gmail.com>
2021-09-19 15:46:10 +0500

    Шапка

--- src/tools/src/app/App.tsx
+++ src/tools/src/app/App.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React from "react";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import { BrowserRouter } from "react-router-dom";
-import { Header } from './Header';
-import { Router } from './Router';
+import { Header } from "./Header";
+import { Router } from "./Router";
+import { MDBContainer } from "mdb-react-ui-kit";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Router />
+      <MDBContainer fluid className="mt-3">
+        <Router />
+      </MDBContainer>
     </BrowserRouter>
   );
 }
--- src/tools/src/app/Header.tsx
+++ src/tools/src/app/Header.tsx
@@ -1,12 +1,59 @@
-import { AllRoutes } from "./Routes"
+import { useState } from "react";
+import { AllRoutes } from "./Routes";
+import {
+    MDBNavbar,
+    MDBContainer,
+    MDBIcon,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarToggler,
+    MDBNavbarBrand,
+    MDBCollapse,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+    MDBDropdownLink
+  } from "mdb-react-ui-kit";
 
 export const Header: React.FC = () => {
+    const [showBasic, setShowBasic] = useState(false);
+
     return (
         <>
-            <div>Header</div>
-            {AllRoutes.map(x => (
-                <><a href={`/tools/${x.url}`}>{x.name}</a> | </>
-            ))}
+            <MDBNavbar expand="lg" dark bgColor="primary">
+                <MDBContainer fluid>
+                    <MDBNavbarBrand href="/">Tools</MDBNavbarBrand>
+
+                    <MDBNavbarToggler
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        onClick={() => setShowBasic(!showBasic)}
+                    >
+                        <MDBIcon icon="bars" fas />
+                    </MDBNavbarToggler>
+
+                    <MDBCollapse navbar show={showBasic}>
+                        <MDBNavbarNav right fullWidth={false} className="mb-2 mb-lg-0">
+                            <MDBNavbarItem>
+                                <MDBDropdown>
+                                    <MDBDropdownToggle tag="a" className="nav-link">
+                                        Choose tools
+                                    </MDBDropdownToggle>
+                                    <MDBDropdownMenu>
+                                        {AllRoutes.map(x => (
+                                            <MDBDropdownItem>
+                                                <MDBDropdownLink href={`/tools/${x.url}`}>{x.name}</MDBDropdownLink>
+                                            </MDBDropdownItem>
+                                        ))}
+                                    </MDBDropdownMenu>
+                                </MDBDropdown>
+                            </MDBNavbarItem>
+                        </MDBNavbarNav>
+                    </MDBCollapse>
+                </MDBContainer>
+            </MDBNavbar>
         </>
     )
 }
