70bc269a54a20898c1051c6db5d27e32276e63a7
Roman Puzanov <puzanovrs@gmail.com>
2021-10-08 12:49:36 +0500

    Расчет времени

--- src/tools/src/tools/unit-converter/UnitConverterUtils.ts
+++ src/tools/src/tools/unit-converter/UnitConverterUtils.ts
@@ -19,14 +19,27 @@ export enum Unit {
 	TB = "Terabyte (MB)",
 	PB = "Petabyte (MB)",
 	EB = "Exabyte (MB)",
+
+	// Time
+	tick = "Tick (t)",
+	ms = "Milissecond (ms)",
+	s = "Second (s)",
+	m = "Minute (m)",
+	h = "Hour (h)",
+	d = "Day (d)",
+	M = "Month (M)",
+	y = "Year (y)",
 }
 
 export enum UnitType {
 	DataStorage = "Data Storage",
+	Time = "Time",
 }
 
 export const UnitGroups: { [unitType: string]: Unit[] } = {
 	[UnitType.DataStorage]: [Unit.bit, Unit.b, Unit.KB, Unit.MB, Unit.GB, Unit.TB, Unit.PB, Unit.EB,],
+	[UnitType.Time]: [Unit.tick, Unit.ms, Unit.s, Unit.m, Unit.h, Unit.d, Unit.M, Unit.y,],
+	// [UnitType.]: [Unit., Unit., Unit., Unit., Unit., Unit., Unit., Unit., Unit., Unit., Unit., Unit., Unit., Unit.,],
 }
 
 export const UnitTypes = Object.keys(UnitGroups);
@@ -41,4 +54,26 @@ const UnitValues: { [unit: string]: number } = {
 	[Unit.TB]: 8 * 1024 * 1024 * 1024 * 1024,
 	[Unit.PB]: 8 * 1024 * 1024 * 1024 * 1024 * 1024,
 	[Unit.EB]: 8 * 1024 * 1024 * 1024 * 1024 * 1024 * 1024,
+
+	// Time
+	[Unit.tick]: 1,
+	[Unit.ms]: 100,
+	[Unit.s]: 100 * 1000,
+	[Unit.m]: 100 * 1000 * 60,
+	[Unit.h]: 100 * 1000 * 60 * 60,
+	[Unit.d]: 100 * 1000 * 60 * 60 * 24,
+	[Unit.M]: 100 * 1000 * 60 * 60 * 24 * 30.43,
+	[Unit.y]: 100 * 1000 * 60 * 60 * 24 * 30.43 * 12,
+
+	// ****
+	// [Unit.]: 1,
+	// [Unit.]: 1000,
+	// [Unit.]: 1000 * 1000,
+	// [Unit.]: 1000 * 1000 * 1000,
+	// [Unit.]: 1000 * 1000 * 1000 * 1000,
+	// [Unit.]: 1000 * 1000 * 1000 * 1000 * 1000,
+	// [Unit.]: 1000 * 1000 * 1000 * 1000 * 1000 * 1000,
+	// [Unit.]: 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000,
+	// [Unit.]: 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000,
+	// [Unit.]: 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000 * 1000,
 }
