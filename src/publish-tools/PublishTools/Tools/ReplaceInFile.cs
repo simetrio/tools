dbaee8b9aaa85bc6b4c706e3879d5ac05dfd2c07
Roman Puzanov <puzanovrs@gmail.com>
2021-10-17 02:02:10 +0500

    Замена в файле

---
+++ src/publish-tools/PublishTools/Tools/ReplaceInFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace PublishTools.Tools
+{
+    public class ReplaceInFile : ITools
+    {
+        public void Execute(CommandLine commandLine)
+        {
+            var name = commandLine.Get("--name");
+            var from = commandLine.Get("--from");
+            var to = commandLine.Get("--to");
+            Console.WriteLine($"Replace in file '{name}' from '{from}' to '{to}'");
+            
+            var text = Encoding.UTF8.GetString(File.ReadAllBytes(name));
+            text = text.Replace(from, to);
+            File.WriteAllBytes(name, Encoding.UTF8.GetBytes(text));
+        }
+    }
+}
--- src/tools/runBuild.bat
+++ src/tools/runBuild.bat
@@ -1,5 +1,6 @@
 /npm run build
 PublishTools.exe -mode DeleteDirectory --name "../../docs"
 PublishTools.exe -mode CopyDirectory --from "build" --to "../../docs"
-PublishTools.exe -mode CreatePages --pages "src/app/Routes.ts" --to "../../docs/tools" --source "../../docs/index.html"
+PublishTools.exe -mode ReplaceInFile --name "../../docs/index.html" --from "{Title}" --to "Olrix Tools"
+PublishTools.exe -mode CreatePages --pages "src/app/Routes.ts" --to "../../docs/tools" --source "build/index.html"
 /pause
